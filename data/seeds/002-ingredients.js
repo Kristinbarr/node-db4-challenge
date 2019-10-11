d29aede7411593bb4dda31e088610e9c0d8ce40f
Kristin Barr <kristinbarr@gmail.com>
2019-10-11 11:05:15 -0400

    MVP - added migration, seed and db files

---
+++ data/seeds/002-ingredients.js
@@ -0,0 +1,26 @@
+exports.seed = function(knex) {
+  return knex('ingredients')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('ingredients').insert([
+        { name: 'flour' },
+        { name: 'butter' },
+        { name: 'sugar' },
+        { name: 'egg' },
+        { name: 'baking soda' },
+        { name: 'onion' },
+        { name: 'celery' },
+        { name: 'carrots' },
+        { name: 'ground beef' },
+        { name: 'all-spice seasoning' },
+        { name: 'milk' },
+        { name: 'tomato' },
+        { name: 'bread' },
+        { name: 'mozzarella' },
+        { name: 'provalone' },
+        { name: 'cheddar' },
+        { name: 'avocado' }
+      ])
+    })
+}
